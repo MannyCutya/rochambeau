4932a422a0b2d604d9b04c22da9e00df549aafd2
Matt Gonzales <mgonzales@connexionpoint.com>
2019-01-14 15:12:17 -0700

    remove trash

--- removeFromArray.js
+++
@@ -1,6 +0,0 @@
-const removeFromArray = function(...args) {
-    const array = args[0]
-    return array.filter(val => !args.includes(val))
-}
-
-module.exports = removeFromArray
--- reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(string) {
-    var splitString = string.split("");
-    var reverseArray = splitString.reverse();
-    var joinArray = reverseArray.join("");
-
-    return joinArray;
-}
-
-module.exports = reverseString
