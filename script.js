66f02d7726d0dcdc61c6299b4bd53398e1d77e2a
Matt Gonzales <mgonzales@connexionpoint.com>
2019-01-15 15:11:05 -0700

    cleanup

--- script.js
+++ script.js
@@ -2,63 +2,60 @@ let playerScore = 0;
 let cpuScore = 0;
 const plays = ['rock', 'paper', 'scissors'];
 
+
 function playRound(playerSelection, element) {
 
-    console.log(element);
+    element.setAttribute('style', 'background-color: white');
     let computerSelection = plays[Math.floor(Math.random()*plays.length)];
 
-    // console.log(`player picked ${playerSelection}`);
-    // console.log(`cpu picked ${computerSelection}`);
-
-    // console.log(checkWinner(playerSelection, computerSelection));
-
-    checkWinner(playerSelection, computerSelection);
-    element.textContent = `Player picked ${playerSelection}  |  CPU picked ${computerSelection}`
+    results = checkWinner(playerSelection, computerSelection);
+    if (results === "Tie") {
+        element.setAttribute('style', 'background-color: yellow');
+        element.textContent = `Both picked ${playerSelection}! Go again!`
+    } else {
+        element.textContent = `Player picked ${playerSelection}  |  CPU picked ${computerSelection}`
+    }
     scorePara.textContent =  `Player: ${playerScore} | CPU: ${cpuScore}`
 
-    if (playerScore === 5) {
-        console.log('Player wins!')
-        element.textContent = "Player wins!";
+    if (playerScore === 5 || cpuScore === 5) {
+        if (results === "Player wins!") {
+            element.setAttribute('style', 'color: white; background-color: green');
+        } else {
+            element.setAttribute('style', 'color: white; background-color: red');
+        }
+        element.textContent = results;
         playerScore = 0;
         cpuScore = 0;
-    } else if (cpuScore === 5) { 
-        console.log('CPU wins!') 
-        element.textContent = "CPU wins!";
-        playerScore = 0;
-        cpuScore = 0;
-    }
+    } 
 }
+
 
 function checkWinner(choice1, choice2) {
 
     choice1 = plays.indexOf(choice1);
     choice2 = plays.indexOf(choice2);
 
-    // console.log(`choice1 is ${choice1}`);
-    // console.log(`choice2 is ${choice2}`);
-
     if (choice1 === choice2) {
         return "Tie";
     }
     if (choice1 === plays.length -1 && choice2 == 0) {
         cpuScore++;
-        return "CPU wins";
+        return "CPU wins!";
     }
     if (choice2 === plays.length -1 && choice1 == 0) {
         playerScore++;
-        return "Player wins";
+        return "Player wins!";
     }
     if (choice1 > choice2) {
         playerScore++;
-        return "Player wins"
+        return "Player wins!"
     } else {
         cpuScore++;
-        return "CPU wins";
+        return "CPU wins!";
     }
-
-
 }
 
+// DOM
 const wrapper = document.getElementById('wrapper');
 const bod = document.querySelector('body');
 const div = document.createElement('div')
@@ -75,6 +72,4 @@ wrapper.addEventListener('click', (event) => {
     if (!isButton) {
         return;
     } else { playRound(event.target.id, para); }
-
-    // console.dir(event.target.id);
 })
